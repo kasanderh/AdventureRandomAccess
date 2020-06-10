4c7567a681c59a2d0154fcb574ea66438dc639ed
Kasanderh <Kasanderh@gmail.com>
2020-06-10 09:50:14 +0200

    Initial commit

---
+++ src/com/kasanderh/Main.java
@@ -0,0 +1,71 @@
+package com.kasanderh;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static Locations locations = new Locations();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("QUIT", "Q");
+
+        int loc = 1;
+        while(true) {
+            System.out.println(locations.get(loc).getDescription());
+            if(loc==0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for(String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+
+//            String direction = scanner.nextLine().toUpperCase();
+//            if(direction.contains("NORTH") || direction == "N") {
+//                direction = "N";
+//            } else if(direction.contains("WEST") || direction == "W") {
+//                direction = "W";
+//            } else if(direction.contains("EAST") || direction == "E") {
+//                direction = "E";
+//            } else if(direction.contains("SOUTH") || direction == "S") {
+//                direction = "S";
+//            } else if(direction.contains("QUIT") || direction == "Q") {
+//                direction = "Q";
+//            } else {
+//                System.out.println("Invalid direction");
+//            }
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction =vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
+
+    }
+}

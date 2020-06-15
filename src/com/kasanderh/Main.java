d720394443dc1fef0f236c13517897fde55046ce
Kasanderh <Kasanderh@gmail.com>
2020-06-15 15:57:03 +0200

    Changed code to work with ObjectIO.

--- src/com/kasanderh/Main.java
+++ src/com/kasanderh/Main.java
@@ -3,7 +3,7 @@ package com.kasanderh;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
+    
 public class Main {
     private static Locations locations = new Locations();
 
